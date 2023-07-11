078e4ed89f84684f9c3068f36e98ea0d77578b5e
VIVIAN NJAU <viviannjau@users.noreply.github.com>
2023-07-11 22:19:06 +0200

    Goods

--- src/components/BarChart.jsx
+++ src/components/BarChart.jsx
@@ -39,7 +39,7 @@ const BarChart = ({ isDashboard = false }) => {
           },
         },
       }}
-      keys={["hot dog", "burger", "sandwich", "kebab", "fries", "donut"]}
+      keys={["Sugar", "Maize", "Cotton", "Bags", "Flour", "Coffee"]}
       indexBy="country"
       margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
       padding={0.3}
